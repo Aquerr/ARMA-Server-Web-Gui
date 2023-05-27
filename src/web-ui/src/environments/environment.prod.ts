b3d9f9650ee391af1a303054b8b4c08f7847a2d5
Aquerr <nerdianin@gmail.com>
2023-05-27 13:37:02 +0200

    Revert "Try changing wss to ws."
    This reverts commit a28d4db4a019da9f4aa3e1c53e720b37763d9fb6.

--- src/web-ui/src/environments/environment.prod.ts
+++ src/web-ui/src/environments/environment.prod.ts
@@ -3,4 +3,4 @@ export const environment = {
 };
 
 export const API_BASE_URL = "https://arma.bartlomiejstepien.pl/api/v1"
-export const API_WS_BASE_URL = "ws://arma.bartlomiejstepien.pl/api/v1/ws";
+export const API_WS_BASE_URL = "wss://arma.bartlomiejstepien.pl/api/v1/ws";
