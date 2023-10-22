cbbd3b75dbbfa879c5567b889edfb9d24f36ac71
Aquerr <nerdianin@gmail.com>
2023-10-22 20:25:52 +0200

    Handle all REST RuntimeExceptions

---
+++ src/main/java/pl/bartlomiejstepien/armaserverwebgui/web/ErrorRestController.java
@@ -0,0 +1,19 @@
+package pl.bartlomiejstepien.armaserverwebgui.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import pl.bartlomiejstepien.armaserverwebgui.web.response.RestErrorResponse;
+
+@RestControllerAdvice
+@Slf4j
+public class ErrorRestController
+{
+    @ExceptionHandler(RuntimeException.class)
+    public RestErrorResponse handleException(RuntimeException runtimeException)
+    {
+        log.error(runtimeException.getMessage(), runtimeException);
+        return RestErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(), HttpStatus.INTERNAL_SERVER_ERROR.value());
+    }
+}
