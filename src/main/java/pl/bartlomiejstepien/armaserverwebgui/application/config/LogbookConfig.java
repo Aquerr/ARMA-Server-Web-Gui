e4a52d16869d76cc322ed36a8125de515d4bee9f
Aquerr <nerdianin@gmail.com>
2023-11-06 12:34:34 +0100

    Skip logging of html and javascript files

--- src/main/java/pl/bartlomiejstepien/armaserverwebgui/application/config/LogbookConfig.java
+++ src/main/java/pl/bartlomiejstepien/armaserverwebgui/application/config/LogbookConfig.java
@@ -5,16 +5,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.server.WebFilter;
 import org.zalando.logbook.Logbook;
+import org.zalando.logbook.core.BodyFilters;
 import org.zalando.logbook.core.DefaultCorrelationId;
 import org.zalando.logbook.core.DefaultHttpLogWriter;
 import org.zalando.logbook.core.DefaultSink;
+import org.zalando.logbook.core.ResponseFilters;
 import org.zalando.logbook.json.FastJsonHttpLogFormatter;
 import org.zalando.logbook.spring.webflux.LogbookWebFilter;
 
+import static org.zalando.logbook.core.Conditions.contentType;
 import static org.zalando.logbook.json.JsonPathBodyFilters.jsonPath;
 
 @Configuration
-public class WebConfiguration implements WebFluxConfigurer
+public class LogbookConfig implements WebFluxConfigurer
 {
     @Bean
     public WebFilter logbookFilter(Logbook logbook)
@@ -26,6 +29,7 @@ public class WebConfiguration implements WebFluxConfigurer
     public Logbook logbook()
     {
         Logbook logbook = Logbook.builder()
+                .responseFilter(ResponseFilters.replaceBody(response -> contentType("text/html", "text/javascript", "application/javascript").test(response) ? "<skipped>" : null))
                 .bodyFilter(jsonPath("$.password").replace("XXX"))
                 .bodyFilter(jsonPath("$.publishedFileDetails").delete())
                 .correlationId(new DefaultCorrelationId())
