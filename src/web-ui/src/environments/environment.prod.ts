a0c99c3672ebf935b7ad78e3ec2f9f19309617fa
Aquerr <nerdianin@gmail.com>
2024-08-18 10:19:32 +0200

    Add placeholders for API_BASE_URL and API_WS_BASE_URL

--- src/web-ui/src/environments/environment.prod.ts
+++ src/web-ui/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true
 };
 
-export const API_BASE_URL = "https://arma.bartlomiejstepien.pl/api/v1"
-export const API_WS_BASE_URL = "wss://arma.bartlomiejstepien.pl/api/v1/ws";
+export const API_BASE_URL = "{API_BASE_URL}"
+export const API_WS_BASE_URL = "{API_WS_BASE_URL}";
