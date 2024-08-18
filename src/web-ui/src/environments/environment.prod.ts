2287a061abc228d3e2e177328653e92670259a57
Aquerr <nerdianin@gmail.com>
2024-08-18 15:12:27 +0200

    Fix token replacement

--- src/web-ui/src/environments/environment.prod.ts
+++ src/web-ui/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true
 };
 
-export const API_BASE_URL = "{API_BASE_URL}"
-export const API_WS_BASE_URL = "{API_WS_BASE_URL}";
+export const API_BASE_URL = "@API_BASE_URL@"
+export const API_WS_BASE_URL = "@API_WS_BASE_URL@";
