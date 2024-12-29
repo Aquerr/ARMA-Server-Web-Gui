f419c49ca57246b0d0986d9e322870577d6fce43
Aquerr <nerdianin@gmail.com>
2024-12-30 00:12:03 +0100

    Fix for ServerHttpRequest from wrong package

--- src/main/java/pl/bartlomiejstepien/armaserverwebgui/web/AuthRestController.java
+++ src/main/java/pl/bartlomiejstepien/armaserverwebgui/web/AuthRestController.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
