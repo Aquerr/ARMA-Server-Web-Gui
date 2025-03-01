f4e29f82c26539c982c55dd13db40ae0b6ad47ab
Aquerr <nerdianin@gmail.com>
2025-03-01 20:53:50 +0100

    Add ApiExceptionCode for bad credentials

--- src/main/java/pl/bartlomiejstepien/armaserverwebgui/application/security/exception/BadCredentialsException.java
+++ src/main/java/pl/bartlomiejstepien/armaserverwebgui/application/security/exception/BadCredentialsException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import pl.bartlomiejstepien.armaserverwebgui.application.ApiException;
 import pl.bartlomiejstepien.armaserverwebgui.application.ApiExceptionCode;
 
-@ApiException(status = HttpStatus.BAD_REQUEST, code = ApiExceptionCode.BAD_AUTH_TOKEN, messageKey = "aswg.security.bad-credentials")
+@ApiException(status = HttpStatus.BAD_REQUEST, code = ApiExceptionCode.BAD_CREDENTIALS, messageKey = "aswg.security.bad-credentials")
 public class BadCredentialsException extends RuntimeException
 {
 
