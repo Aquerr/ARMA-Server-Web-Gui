4f60010c1d91f090555f143210cbbe4ea4d74a98
Aquerr <nerdianin@gmail.com>
2025-03-23 12:09:12 +0100

    Add message key for steamcmd not being installed

--- src/main/java/pl/bartlomiejstepien/armaserverwebgui/domain/steam/exception/SteamCmdNotInstalled.java
+++ src/main/java/pl/bartlomiejstepien/armaserverwebgui/domain/steam/exception/SteamCmdNotInstalled.java
@@ -1,5 +1,10 @@
 package pl.bartlomiejstepien.armaserverwebgui.domain.steam.exception;
 
+import org.springframework.http.HttpStatus;
+import pl.bartlomiejstepien.armaserverwebgui.application.ApiException;
+import pl.bartlomiejstepien.armaserverwebgui.application.ApiExceptionCode;
+
+@ApiException(status = HttpStatus.INTERNAL_SERVER_ERROR, code = ApiExceptionCode.STEAM_CMD_NOT_INSTALLED, messageKey = "aswg.steam.not-installed")
 public class SteamCmdNotInstalled extends RuntimeException
 {
 
