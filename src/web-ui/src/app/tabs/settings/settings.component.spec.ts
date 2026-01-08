8294a6ea4c4264278a1b176af6260b9031d40ee6
Aquerr <nerdianin@gmail.com>
2026-01-08 21:03:06 +0100

    Small improvement for SettingsComponent test

--- src/web-ui/src/app/tabs/settings/settings.component.spec.ts
+++ src/web-ui/src/app/tabs/settings/settings.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { SettingsComponent } from "./settings.component";
 import { provideRouter } from "@angular/router";
+import { IconRegistrarService } from "../../service/icon-registrar.service";
 
 describe("SettingsComponent", () => {
   let component: SettingsComponent;
@@ -12,6 +13,8 @@ describe("SettingsComponent", () => {
       imports: [SettingsComponent],
       providers: [provideRouter([])]
     });
+
+    TestBed.inject(IconRegistrarService); // To load aswg icons
     fixture = TestBed.createComponent(SettingsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
