6d7520e38d63dbb029867dfdb699ad8abac327eb
Aquerr <nerdianin@gmail.com>
2026-01-26 15:10:33 +0100

    Fix test in DifficultyPanelComponent

--- src/web-ui/src/app/tabs/difficulty/difficulty-panel/difficulty-panel.component.spec.ts
+++ src/web-ui/src/app/tabs/difficulty/difficulty-panel/difficulty-panel.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { DifficultyPanelComponent } from "./difficulty-panel.component";
 import { provideToastr } from "ngx-toastr";
 import { DifficultyProfile } from "../../../model/difficulty-profile.model";
+import { inputBinding } from "@angular/core";
 
 describe("DifficultyPanelComponent", () => {
   let component: DifficultyPanelComponent;
@@ -14,14 +15,19 @@ describe("DifficultyPanelComponent", () => {
       providers: [provideToastr()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(DifficultyPanelComponent);
+    fixture = TestBed.createComponent(DifficultyPanelComponent, {
+      bindings: [
+        inputBinding("difficultyProfile", () => {
+          return {
+            id: 1,
+            name: "test",
+            active: true,
+            options: {}
+          } as DifficultyProfile;
+        })
+      ]
+    });
     component = fixture.componentInstance;
-    component.difficultyProfile = {
-      id: 1,
-      name: "test",
-      active: true,
-      options: {}
-    } as DifficultyProfile;
     fixture.detectChanges();
   });
 
